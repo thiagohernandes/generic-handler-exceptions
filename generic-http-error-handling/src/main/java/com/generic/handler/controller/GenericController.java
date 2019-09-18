bdefc1f14fdc81974255675818cd3e1a1c74145c
Thiago Hernandes Souza <thiago.hernandes.souza@everis.com>
2019-09-18 09:17:39 -0300

    Not found exception

--- generic-http-error-handling/src/main/java/com/generic/handler/controller/GenericController.java
+++ generic-http-error-handling/src/main/java/com/generic/handler/controller/GenericController.java
@@ -4,8 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.generic.handler.filter.exceptions.ExceptionUnauthorized;
 import com.generic.handler.filter.exceptions.ExceptionNotImplemented;
+import com.generic.handler.filter.exceptions.ExceptionUnauthorized;
+import com.generic.handler.filter.exceptions.ExceptionNotFound;
 
 @RestController
 @RequestMapping("/api")
@@ -24,6 +25,11 @@ public class GenericController {
 	@GetMapping("/erro2")
 	public String teste3() {
 		throw new ExceptionNotImplemented("Sem implementação");
+	}
+	
+	@GetMapping("/erro4")
+	public String teste4() {
+		throw new ExceptionNotFound("Não encontrado");
 	}
 
 }
